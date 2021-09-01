0b2eb00389010910dcee81ac940d84cd96d96143
Brijesh Bhalala <32086209+Brijesh619@users.noreply.github.com>
2021-09-01 16:01:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends React.Component {
+  render(){
+  return (
+    <h1>Todo List</h1>
+    
+   
+  )
+}};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+export default App;
+
